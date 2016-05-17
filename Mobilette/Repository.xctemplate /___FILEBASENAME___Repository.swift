3e1f92f20b099fd4b8494355ce82f2372f19901c
Romain ASNAR <romain.asnar@gmail.com>
2016-05-17 20:28:52 +0200

    Add repository template.

---
+++ Mobilette/Repository.xctemplate /___FILEBASENAME___Repository.swift
@@ -0,0 +1,63 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___Repository:
+    Equatable,
+    CustomStringConvertible
+{
+    // MARK: - Property
+    
+    private var <# property name #>s: [___FILEBASENAMEASIDENTIFIER___] = []
+    
+    // MARK: - Collection
+    
+    func add___FILEBASENAMEASIDENTIFIER___(<# property name #>: ___FILEBASENAMEASIDENTIFIER___)
+    {
+        self.<# property name #>s.append(<# property name #>)
+    }
+    
+    func add___FILEBASENAMEASIDENTIFIER___s(<# property name #>s: [___FILEBASENAMEASIDENTIFIER___])
+    {
+        self.<# property name #>s.appendContentsOf(<# property name #>s)
+    }
+    
+    func update___FILEBASENAMEASIDENTIFIER___(<# property name #>: ___FILEBASENAMEASIDENTIFIER___)
+    {
+        if let index = self.<# property name #>s.indexOf({ $0.identifier == <# property name #>.identifier }) {
+            self.<# property name #>s[index] = card
+        }
+    }
+    
+    func remove___FILEBASENAMEASIDENTIFIER___(identifer id: String)
+    {
+        self.<# property name #>s = self.<# property name #>s.filter {
+            return $0.identifier != id
+        }
+    }
+    
+    func removeAll___FILEBASENAMEASIDENTIFIER___s()
+    {
+        self.<# property name #>s = []
+    }
+    
+    // MARK: - Printable protocol
+    
+    var description: String {
+        return "{ ___FILEBASENAMEASIDENTIFIER___Repository" + "\n"
+            + "<# property name #>: \(self.<# property name #>)" + "\n"
+            + "}" + "\n"
+    }
+}
+
+// MARK: - Equatable protocol
+
+func ==(lhs: ___FILEBASENAMEASIDENTIFIER___Repository, rhs: ___FILEBASENAMEASIDENTIFIER___Repository) -> Bool {
+    return lhs.cards == rhs.cards
+}
