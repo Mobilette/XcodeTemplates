734dec98d9a92b24d683fac026856d405f0f3057
Romain ASNAR <romain.asnar@gmail.com>
2017-02-23 21:13:45 +0100

    Update template version

--- File Templates/Mobilette/Service.xctemplate/NetworkhasInOut/___FILEBASENAME___Controller.swift
+++ File Templates/Mobilette/Service.xctemplate/NetworkhasInOut/___FILEBASENAME___Controller.swift
@@ -2,7 +2,7 @@
 //  ___FILENAME___
 //  ___PROJECTNAME___
 //
-//  Mobilette template version 2.0
+//  Mobilette template version 4.0
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //___COPYRIGHT___
