bdf767f5916c4467731a88138e23857bd5bf2927
Romain ASNAR <romain.asnar@gmail.com>
2017-03-04 10:31:31 +0100

    Update version template to 4.1.

--- File Templates/Mobilette/UI.xctemplate/UIViewController/___FILEBASENAME___Presenter.swift
+++ File Templates/Mobilette/UI.xctemplate/UIViewController/___FILEBASENAME___Presenter.swift
@@ -2,7 +2,7 @@
 //  ___FILENAME___
 //  ___PROJECTNAME___
 //
-//  ACMS template version 2.0
+//  Mobilette template version 4.1
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 // ___COPYRIGHT___
